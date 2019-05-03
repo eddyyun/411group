670fd29b9c704466bd00d9f1a4b175bf0cba133e
Mohammad Ali Al-Rajjal <mohd.ali.rijjal@gmail.com>
2019-05-03 18:27:52 -0400

    Update urls.py

--- cs411_project/dateapp/urls.py
+++ cs411_project/dateapp/urls.py
@@ -6,8 +6,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('history',views.history,name='history'),
-    # path('logout', views.logout,name='logout'),
-    # path('new_epipen/', views.add_epipen, name='new_epipen'),
-    # path('edit_profile/', views.edit_profile, name='edit_profile'),
-    # path('view_history/', views.emergency_history, name='view_history'),
     ]
